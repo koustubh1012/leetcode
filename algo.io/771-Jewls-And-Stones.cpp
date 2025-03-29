45146a4d81a4a4a234d74e74691c651398d9ea6f
koustubh1012 <koustubh1012@gmail.com>
2025-03-28 23:39:46 -0400

    771-optimised approach

--- algo.io/771-Jewls-And-Stones.cpp
+++ algo.io/771-Jewls-And-Stones.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
-public:
-    int numJewelsInStones(string jewels, string stones) {
-        int count = 0;
-        for (int i=0; i<jewels.length(); i++){
-            for (int j=0; j<stones.length(); j++){
-                if (jewels[i] == stones[j]){
+    public:
+        int numJewelsInStones(string jewels, string stones) {
+            int count = 0;
+            unordered_set<char> jewel_set(jewels.begin(), jewels.end());
+            for (char stone : stones) {
+                if (jewel_set.count(stone)) { // Check if stone is a jewel
                     count++;
                 }
             }
+            return count;
         }
-        return count;
-    }
-};
+    };
 
 int main() {
     Solution sol;
