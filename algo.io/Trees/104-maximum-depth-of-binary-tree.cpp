9abf3b3a2eb7865fc75e164f3397f84942eaa4f7
koustubh1012 <koustubh1012@gmail.com>
2025-04-07 18:00:40 -0400

    104-completed

---
+++ algo.io/Trees/104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    public:
+        int maxDepth(TreeNode* root) {
+            if(root == nullptr){
+                return 0;
+            }
+            int L = maxDepth(root->left);
+            int R = maxDepth(root->right);
+            return 1 + max(L, R);
+        }
+    };
+
+int main() {
+    Solution solution;
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    int depth = solution.maxDepth(root);
+    cout << "Maximum depth of the binary tree: " << depth << endl;
+
+    return 0;
+}
+
+
