ac9e4ccaa20558c8906099f550a9c68d804c2ac7
Koustubh <36124868+koustubh1012@users.noreply.github.com>
2025-08-20 11:28:01 -0400

    Create 20-valid-parentheses.cpp

---
+++ algo.io/Stacks/20-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c: s){
+            if(c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            }
+            else{
+                if(stk.empty()) return false;
+                if(c == ')' && stk.top() == '('){
+                    stk.pop();
+                }
+                else if(c == ']' && stk.top() == '['){
+                    stk.pop();
+                }
+                else if(c == '}' && stk.top() == '{'){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!stk.empty()) return false;
+        return true;
+    }
+};
